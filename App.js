e73baddd10f4ba148685067a142ec13d6c93e110
Chirag069 <107105460+Chirag069@users.noreply.github.com>
2022-08-20 22:51:38 +0530

    header

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React,{useEffect} from 'react'
 import SplashScreen from 'react-native-splash-screen'
 import StackNavigation from './src/components/Navigation/StackNavigation'
@@ -13,6 +13,7 @@ const App = () => {
   
   return (
     <Provider store={store}>
+      <StatusBar  backgroundColor={"#c79248"} />
     <StackNavigation/>
     <Toast ref={ref => Toast.setRef(ref)} />
     </Provider>
--- src/components/Navigation/StackNavigation.js
+++ src/components/Navigation/StackNavigation.js
@@ -1,14 +1,18 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
+import { Button, View,Image, Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Spinner from 'react-native-loading-spinner-overlay';
+import {useSelector} from 'react-redux';
 //screens
 import CustomerOption from '../CustomerOption'
 import SignIn from '../../screens/SignIn';
 import SignUp from '../../screens/SignUp';
 import Home from '../../screens/Home';
-import {useSelector} from 'react-redux';
+
+//icons
+import Entypo from 'react-native-vector-icons/Entypo'
+import AntDesign from 'react-native-vector-icons/AntDesign'
 
 const Stack = createNativeStackNavigator();
 
@@ -28,15 +32,35 @@ function MyStack() {
 
     <Stack.Navigator
     screenOptions={{
-        headerShown:false
+        headerShown:false,
+
     }}>
         {userToken ? (
-       <Stack.Screen name="Home" component={Home} />
+       <Stack.Screen 
+       options={{
+        headerShown:true,
+        header:()=>(
+          <View style={{backgroundColor:"#c79248",paddingHorizontal:10,flexDirection:"row",justifyContent:"space-between" }}>
+            <View style={{flexDirection:"row"}}>
+              <Pressable>
+            <Entypo name='list'size={35} color={"white"}  />
+            </Pressable>
+            <Image style={{height:40,width:130,marginTop:5}} source={require('../../../assets/Images/Raging-Gold.png')}/>
+            </View>
+            <Pressable>
+              <AntDesign name='search1' size={30} color="white"/>
+            </Pressable>
+          </View>
+        )
+      }}
+       name="Home" 
+       component={Home} />
        ) : (
         <>
       <Stack.Screen name="CustomerOption" component={CustomerOption} />
       <Stack.Screen name="SignIn" component={SignIn} />
-      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen
+       name="SignUp" component={SignUp} />
       </>
       )}
     </Stack.Navigator>
